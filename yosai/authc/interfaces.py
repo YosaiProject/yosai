6f5946d490095c4d76452ddae81ad945d4eda622
Darin Gordon <dkgdkg@gmail.com>
2015-04-14 14:53:18 -0400

    authc reconciliation

--- yosai/authc/interfaces.py
+++ yosai/authc/interfaces.py
@@ -15,17 +15,47 @@ class ABCAuthenticationEvent(Event, metaclass=ABCMeta):
 
 
 class IAuthenticationListener(metaclass=ABCMeta):
+    """
+     An AuthenticationListener listens for notifications while Subjects 
+     authenticate with the system.
+    """
 
     @abstractmethod
     def on_success(self, authc_token, authc_info):
+        """
+        Callback triggered when an authentication attempt for a Subject  
+        succeeds
+         
+        :param authc_token: the authentication token submitted during the 
+                            Subject (user)'s authentication attempt
+        :param authc_info:  the authentication-related account data acquired
+                            after authentication for the corresponding Subject
+        """
         pass
 
     @abstractmethod
     def on_failure(self, authc_token, authc_exception):
+        """
+        Callback triggered when an authentication attempt for a Subject fails 
+        
+        :param authc_token: the authentication token submitted during the
+                            Subject (user)'s authentication attempt
+        :param authc_exception: the AuthenticationException that occurred as 
+                                a result of the attempt
+        """
         pass
 
     @abstractmethod
     def on_logout(self, principals):
+        """
+        Callback triggered when a {@code Subject} logs-out of the system.
+        
+        This method will only be triggered when a Subject explicitly logs-out
+        of the session.  It will not be triggered if their Session times out.
+      
+        :param principals: the identifying principals of the Subject logging
+                           out.
+        """
         pass
 
 
@@ -34,18 +64,32 @@ class IAuthenticationToken(metaclass=ABCMeta):
     @property
     @abstractmethod
     def principal(self):
+        """
+        Returns the account identity submitted during the authentication
+        process.  
+        """
         pass
 
     @property
     @abstractmethod
     def credentials(self):
+        """
+        Returns the credentials submitted by the user during the authentication
+        process that verifies the submitted principal account identity.
+        """
         pass
 
 
 class IAuthenticator (metaclass=ABCMeta):
+    """
+    Authenticates an account based on the submitted AuthenticationToken.
+    """
  
     @abstractmethod
     def authenticate_account(self, authc_token):
+        """
+        Authenticates an account based on the submitted AuthenticationToken
+        """
         pass
 
 
