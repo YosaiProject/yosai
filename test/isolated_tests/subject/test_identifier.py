5515ba9cde24d5774f97e084e32ef1bd05a76fbb
dowwie <dkcdkg@gmail.com>
2015-12-21 13:17:52 -0500

    done unit testing subject identifier

--- test/isolated_tests/subject/test_identifier.py
+++ test/isolated_tests/subject/test_identifier.py
@@ -5,69 +5,67 @@ from yosai.core import (
 import collections
 
 
-def test_sic_primary_identifiers_property(simple_identifiers_collection):
+def test_sic_primary_identifier_property(simple_identifiers_collection):
     """
-    unit tested:  primary_identifiers
+    unit tested:  primary_identifier
 
     test case:
     the primary identifiers is initially lazy loaded
     """
     sic = simple_identifiers_collection
-    assert sic.primary_identifiers == 'username'
+    assert sic.primary_identifier == 'username'
 
 
-def test_sic_primary_identifiers_property_exists(simple_identifiers_collection):
+def test_sic_primary_identifier_property_exists(simple_identifiers_collection):
     """
-    unit tested:  primary_identifiers
+    unit tested:  primary_identifier
 
     test case:
     the primary identifiers is initially lazy loaded only if the
-    _primary_identifiers attribute isn't already set
+    _primary_identifier attribute isn't already set
     """
     sic = simple_identifiers_collection
-    sic._primary_identifiers = 'primary1'
-    assert sic.primary_identifiers == 'primary1'
+    sic._primary_identifier = 'primary1'
+    assert sic.primary_identifier == 'primary1'
 
 
-def test_sic_primary_identifiers_property_raises(
+def test_sic_primary_identifier_property_raises(
         simple_identifiers_collection, capsys, monkeypatch):
     """
-    unit tested:  primary_identifiers
+    unit tested:  primary_identifier
 
     test case:
     the primary identifiers is initially lazy loaded
     """
     sic = simple_identifiers_collection
-    monkeypatch.delattr(sic, 'realm_identifiers')
-    result = sic.primary_identifiers
+    monkeypatch.delattr(sic, 'source_identifiers')
+    result = sic.primary_identifier
     out, err = capsys.readouterr()
-    assert (result is None and
-            "failed to arbitrarily obtain" in out)
+    assert result is None
 
 
-@pytest.mark.parametrize('realm_name, identifiers, collection',
+@pytest.mark.parametrize('source_name, identifiers, collection',
                          [('realm1', 'identifiers1', None),
-                          ('realm1', {'identifiers1', 'identifiers2'}, None),
                           (None, None,
                            SimpleIdentifierCollection('realm2', 'identifiers1'))
                           ])
-def test_sic_add(realm_name, identifiers, collection):
+def test_sic_add(source_name, identifiers, collection):
     """
     unit tested:  add
 
     test case:
     the add method accepts two forms of input:
-    1) a realm_name/identifiers pair
+    1) a source_name/identifiers pair
         - either a scalar identifiers value or collection of identifiers (set)
     2) an identifiers collection object
     """
-    sic = SimpleIdentifierCollection(realm_name, identifiers, collection)
+    sic = SimpleIdentifierCollection(source_name, identifiers, collection)
     if collection:
-        assert sic.realm_identifiers == collection.realm_identifiers
+        assert sic.source_identifiers == collection.source_identifiers
     elif isinstance(identifiers, set):
-        assert sic.realm_identifiers[realm_name] == identifiers
+        assert sic.source_identifiers[source_name] == identifiers
     else:
-        assert sic.realm_identifiers[realm_name] == {identifiers}
+        assert sic.source_identifiers[source_name] == identifiers
 
 
 def test_sic_by_type():
@@ -77,37 +75,33 @@ def test_sic_by_type():
     test case:
     returns all identifiers of a requested type
     """
-    DumbClass = type('DumbClass', (object,), {})
-    dc1 = DumbClass()
-    dc2 = DumbClass()
-    identifiers = {dc1, dc2, 'identifiers3', 'id4'}
-    sic = SimpleIdentifierCollection(realm_name='realm1',
-                                     identifiers=identifiers)
-    result = set(sic.by_type(DumbClass))  # convert to set to always match
-    assert {dc1, dc2} == result
+    sic = SimpleIdentifierCollection(source_name='realm1',
+                                     identifier='identifier')
+    result = set(sic.by_type(str))  # convert to set to always match
+    assert result == frozenset(['identifier'])
 
 
-def test_sic_from_realm(simple_identifiers_collection):
+def test_sic_from_source(simple_identifiers_collection):
     """
-    unit tested:  from_realm
+    unit tested:  from_source
 
     test case:
     returns identifiers for realm of interest
     """
     sic = simple_identifiers_collection
-    result = sic.from_realm('realm1')
-    assert result == {'username'}
+    result = sic.from_source('realm1')
+    assert result == 'username'
 
 
-def test_sic_realm_names(simple_identifiers_collection):
+def test_sic_source_names(simple_identifiers_collection):
     """
-    unit tested:  realm_names
+    unit tested:  source_names
 
     test case:
     returns a tuple of realm names
     """
     sic = simple_identifiers_collection
-    result = sic.realm_names
+    result = sic.source_names
     assert result == tuple(['realm1'])
 
 
@@ -120,7 +114,7 @@ def test_sic_is_empty(simple_identifiers_collection):
     """
     sic = simple_identifiers_collection
     assert not sic.is_empty
-    sic.realm_identifiers = collections.defaultdict(set)  # clear()
+    sic.source_identifiers = collections.defaultdict(set)  # clear()
     assert sic.is_empty
 
 
@@ -175,4 +169,4 @@ def test_sic_deserialize(sic_serialized, simple_identifiers_collection):
 
     sic = simple_identifiers_collection
     deserialized = SimpleIdentifierCollection.deserialize(sic_serialized)
-    assert deserialized.realm_identifiers == sic.realm_identifiers
+    assert deserialized.source_identifiers == sic.source_identifiers
