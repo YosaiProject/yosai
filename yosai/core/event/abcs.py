3508b5732d8d83bbf5436128491a50e887922fc4
dowwie <dkcdkg@gmail.com>
2016-01-02 07:17:13 -0500

    documented event

--- yosai/core/event/abcs.py
+++ yosai/core/event/abcs.py
@@ -6,7 +6,7 @@ regarding copyright ownership.  The ASF licenses this file
 to you under the Apache License, Version 2.0 (the
 "License"); you may not use this file except in compliance
 with the License.  You may obtain a copy of the License at
- 
+
     http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing,
@@ -21,22 +21,60 @@ from abc import ABCMeta, abstractmethod
 
 
 class EventBus(metaclass=ABCMeta):
+    """
+    An event bus can publish events to event subscribers as well as provide a
+    mechanism for registering and unregistering event subscribers.
+
+    An event bus enables a publish/subscribe paradigm within Yosai -- components
+    can publish or consume events they find relevant without needing to be
+    tightly coupled to other components.  This affords great flexibility within
+    Yosai by promoting loose coupling and high cohesion between components and
+    a much safer pluggable architecture.
+
+    Sending Events
+    -----------------
+    If a component wishes to publish events to other components:::
+
+        event_bus.publish(topic, *kwargs)
+
+    The event bus dispatches the event 'message' to components that wish to receive
+    events of that type (known as subscribers).
+
+    Receiving Events
+    ------------------
+    A component can receive events of interest by doing the following.
+
+    For each event topic you wish to consume, create a callback method
+    that will be called when an specific type of event is communicated across
+    the event bus.  Register the callback with the event_bus:::
+
+       eventBus.register(topic, callback)
+
+    """
 
     @abstractmethod
-    def publish(self, event):
+    def publish(self, topic_name, **kwargs):
         pass
 
     @abstractmethod
-    def register(self, subscriber):
+    def register(self, _callable, topic_name):
         pass
 
     @abstractmethod
-    def unregister(self, subscriber):
+    def unregister(self, listener, topic_name):
         pass
 
 
 class EventBusAware(metaclass=ABCMeta):
-    
+    """
+    This interface is implemented by components that utilize an EventBus for publishing
+    and/or subscribing to/from events and wish for EventBus to be supplied if
+    one is available.
+
+    **NOTE** If an ``EventBusAware`` implementation wishes to subscribe to events,
+     it must register callbacks with the event bus.
+    """
+
     @property
     @abstractmethod
     def event_bus():
