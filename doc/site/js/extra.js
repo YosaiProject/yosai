56bec3057868ff0e140524efaae6e7dd649be25b
dowwie <dkcdkg@gmail.com>
2016-02-19 13:55:28 -0500

    removed doc/site

--- doc/site/js/extra.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-  var snowplowTracker = Snowplow.getTrackerUrl('derecho.elijah.cs.cmu.edu:8080');
-  snowplowTracker.enableLinkTracking();
-  snowplowTracker.trackPageView();
-}
